e0816d38275f10916d6fb357fa7c98ed73ed4f4d
davepodgorski <davepodgorski@gmail.com>
2018-12-04 17:37:43 -0500

    les surprises

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,7 +8,38 @@ class EntriesController < ApplicationController
   def new
     render :new
   end
+
   def create
-    redirect_to entries_url
+      @entry = Article.new
+
+      @entry.title = params[:article][:title]
+      @entry.text = params[:article][:text]
+
+
+        if @entry.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+          redirect_to_entries_url
+        else
+      # otherwise render new.html.erb
+          render :new
+        end
+      end
+
+      def edit
+        @entry = Article.find(params[:id])
+      end
+
+  def update
+    @entry = Article.find(params[:id])
+
+    @entry.title = params[:article][:title]
+    @entry.text = params[:article][:text]
+
+    if @entry.save
+    redirect_to entry_url(params[:id])
+    else
+      render :edit
+    end
   end
+
 end
